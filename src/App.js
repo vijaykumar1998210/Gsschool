e279f7a6d7189bb14755c5e84b9d630cec202cd1
vijaykumar1998210 <56931350+vijaykumar1998210@users.noreply.github.com>
2021-03-30 18:52:50 +0530

    9th attempt

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import Photo from "./Photo";
@@ -7,50 +7,42 @@ import Table2 from "./Table2";
 import Table3 from "./Table3";
 import Table4 from "./Table4";
 import TeacherRemarks from "./TeacherRemarks";
-import { useReactToPrint } from "react-to-print";
+
 import TeacherSign from "./TeacherSign";
 
-export class App extends React.PureComponent {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <div className="photo_container"></div>
-        <Table1 />
-        <Table2 />
-        <Table3 />
-        <TeacherRemarks />
-        <Table4 />
-        <TeacherSign />
-
-        <div
-          className="bg_gradient"
-          style={{
-            backgroundImage: `Url(${
-              process.env.PUBLIC_URL +
-              "/Images/IMG-20210216-WA0010-removebg-preview.png"
-            })`,
-            backgroundRepeat: "no-repeat",
-          }}
-        ></div>
-      </div>
-    );
-  }
-}
-
-const Example1 = () => {
-  const componentRef = useRef();
-  const handlePrint = useReactToPrint({
-    content: () => componentRef.current,
-  });
-
+function App() {
+  const [hide, setHide] = useState(true);
   return (
-    <div className="printout">
-      <App ref={componentRef} />
-      {/* <button className="print" onClick={handlePrint} style={{}}>
-        Print this out!
-      </button> */}
+    <div className="App">
+      <Header />
+      <div
+        className="photo_container"
+        onDoubleClick={() => {
+          setHide(!hide);
+        }}
+      >
+        {/* <button className="photo-btn">hello</button> */}
+
+        {hide ? <Photo /> : null}
+      </div>
+      <Table1 />
+      <Table2 />
+      <Table3 />
+      <TeacherRemarks />
+      <Table4 />
+      <TeacherSign />
+
+      <div
+        className="bg_gradient"
+        style={{
+          backgroundImage: `Url(${
+            process.env.PUBLIC_URL +
+            "/Images/IMG-20210216-WA0010-removebg-preview.png"
+          })`,
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
     </div>
   );
-};
-export default Example1;
+}
+export default App;
